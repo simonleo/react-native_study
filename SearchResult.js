931d2a00c897551a7a05d8f0704ef95ad08e134e
liushu <liushu740010494@126.com>
2015-11-17 14:12:05 +0800

    show Views

--- SearchResult.js
+++ SearchResult.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var React = require('react-native');
+var PropertyView = require('./PropertyView');
+
 var {
   StyleSheet,
   Image,
@@ -22,14 +24,34 @@ class SearchResults extends Component {
   }
 
   renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
     return (
-      <TouchableHighlight
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.guid)}
         underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{uri: rowData.img_url}}/>
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>£{price}</Text>
+              <Text style={styles.title}
+                    numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
     );
+  }
+
+  rowPressed(propertyGuid) {
+    var property = this.props.listings.filter(prop => prop.guid === propertyGuid)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
 
   render() {
@@ -40,5 +62,33 @@ class SearchResults extends Component {
     );
   }
 }
+
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 module.exports = SearchResults;
